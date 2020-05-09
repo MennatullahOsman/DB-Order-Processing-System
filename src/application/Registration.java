37939999fa9b00c668d763f3c25227e26500bf06
YomnaGamal <yomnagamaleldin922@gmail.com>
2020-05-09 07:42:43 +0200

    REGISTRATION UPDATED NOT FINISHED

--- src/application/Registration.java
+++ src/application/Registration.java
@@ -1,18 +1,21 @@
 package application;
 
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Window;
 
 public class Registration {
-
+	  @FXML
+	private Text userError;
 	@FXML
     private TextField firstname;
 
@@ -47,42 +50,46 @@ public class Registration {
     private TextField shippingadd;
 
     @FXML
-    public void register(ActionEvent event) throws SQLException {
+    public void reg(ActionEvent event) throws SQLException {
 
         Window owner = signup.getScene().getWindow();
-
+        /// ERROR IF IT LEFT EMPTY
         if (usernameup.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter your username");
             return;
         }
-
-        if (firstname.getText().isEmpty()) {
+        /// ERROR IF IT LEFT EMPTY OR CONTAINS A NUMBER
+        if (firstname.getText().isEmpty() || firstname.getText().matches(".*\\d.*")) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter your first name");
             return;
         }
-        
-        if (lastname.getText().isEmpty()) {
+        /// ERROR IF IT LEFT EMPTY OR CONTAINS A NUMBER
+        if (lastname.getText().isEmpty() || lastname.getText().matches(".*\\d.*")) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter your last name");
             return;
         }
-        if (email.getText().isEmpty()) {
+        /// ERROR IF IT LEFT EMPTY OR CONTAINS UN VALID EMAIL ADDRESS
+        if (email.getText().isEmpty() || !validEmail(email.getText())) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter your email address");
             return;
         }
-        if (phone.getText().isEmpty()) {
+        /// ERROR IF IT LEFT EMPTY OR CONTAINS NON NUMERIC VALUE
+        if (phone.getText().isEmpty() || !isNumeric(phone.getText())) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                "Please enter your phone");
+                "Please enter a valid phone number");
             return;
         }
+        /// ERROR IF IT LEFT EMPTY
         if (passwordup.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter a password");
             return;
         }
+        /// ERROR IF IT LEFT EMPTY
         if (shippingadd.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                 "Please enter the shipping address");
@@ -97,10 +104,61 @@ public class Registration {
         String phonen = phone.getText();
         String shippingadds = shippingadd.getText();
         DBConnector DB = new DBConnector();
-        DB.insertRecord(userName, password, firstName, lastName, emailadd, phonen, shippingadds);
+        /// ERROR WHEN TRY TO ENTER ALREADY EXISTING USERNAME
+        if (DB.existusername(userName)) {
+        	showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter another username as this user name already exist!");
+                return;
+        } else {
+        	DB.insertRecord(userName, password, firstName, lastName, emailadd, phonen, shippingadds);
+        	showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
+                    "Welcome " + firstname.getText());
+        }
+    }
 
-        showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
-            "Welcome " + firstname.getText());
+    @FXML
+    public void login(ActionEvent event) throws SQLException {
+
+        Window owner = signin.getScene().getWindow();
+
+        System.out.println(usernamein.getText());
+        System.out.println(passwordin.getText());
+        /// ERROR IF IT LEFT EMPTY
+        if (usernamein.getText().isEmpty()) {
+        	userError.setVisible(true);
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter your user name");
+            return;
+        }
+        /// ERROR IF IT LEFT EMPTY
+        if (passwordin.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter a password");
+            return;
+        }
+
+        String username = usernamein.getText();
+        String password = passwordin.getText();
+
+        DBConnector DB = new DBConnector();
+        boolean flag = DB.validate(username, password);
+
+        if (!flag) {
+            infoBox("Please enter correct User Name and Password", null, "Failed");
+        } else {
+            infoBox("Login Successful!", null, "Failed");
+        }
+    }
+//    @FXML
+//    void testForUserName(ActionEvent event) {
+//    	userError.setVisible(false);
+//    }
+    public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
     }
 
     private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
@@ -110,5 +168,25 @@ public class Registration {
         alert.setContentText(message);
         alert.initOwner(owner);
         alert.show();
+    }
+    public static boolean isNumeric(String str) { 
+    	  try {  
+    	    Double.parseDouble(str);  
+    	    return true;
+    	  } catch(NumberFormatException e){  
+    	    return false;  
+    	  }  
+    	}
+    public static boolean validEmail(String email) 
+    { 
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                            "[a-zA-Z0-9_+&*-]+)*@" + 
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                            "A-Z]{2,7}$"; 
+                              
+        Pattern pat = Pattern.compile(emailRegex); 
+        if (email == null) 
+            return false; 
+        return pat.matcher(email).matches(); 
     }
 }
